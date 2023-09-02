78bf12a7c683e60fb83154ea0e8631bb3e338a0b
Riffe007 <tracietwiford@Tracies-iMac.attlocal.net>
2023-09-02 13:40:06 -0700

    Adding to  the frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div>Hello</div>
+      </div>
+    </>
+  )
+}
+
+export default App
